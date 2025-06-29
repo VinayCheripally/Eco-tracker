b5023f57ef5be7b82395b8a64cdd2e4603f2d45e
VinayCheripally <vinaycheripally1@gmail.com>
2025-06-29 22:30:19 +0530

    add

--- app.config.js
+++ app.config.js
@@ -2,12 +2,19 @@ import 'dotenv/config';
 
 export default {
   expo: {
-    name: 'bolt-expo-nativewind',
-    slug: 'bolt-expo-nativewind',
+    owner: 'vinayhjejiner',
+    name: 'eco-tracker',
+    slug: 'eco-tracker',
     version: '1.0.0',
+    android: {
+      package: 'com.yourname.ecotracker',
+    },
     extra: {
       supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
       supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
+      eas: {
+        projectId: '9e36d89f-6aa3-40aa-bd3d-997c516f6330',
+      },
     },
     orientation: 'portrait',
     icon: './assets/images/icon.png',
